eb8028d687652722e0102253dd984315e287a54e
Emmanuel <to_be_determined>
2024-11-22 12:27:55 +0100

    rererecorrection

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -32,16 +32,6 @@ print('Films with more votes: \n%s' % numberRatings[:10])
 print('\n==================================================================\n')
 
 # Show avg ratings movie (groupby + avg)
-<<<<<<< HEAD
-=======
-avgRatings = cloneDF(mergeRatings)
-avgRatings = avgRatings.groupby(['movie_id', 'title']).mean(numeric_only=True)
-print('Avg ratings: \n%s' % avgRatings['rating'][:10])
-print('\n==================================================================\n')
-
-
-# Show data ratings movies (groupby + several funtions)
->>>>>>> 22a35241b94ff71766ed79d0af0aa5c452267d18
 dataRatings = cloneDF(mergeRatings)
 dataRatings = dataRatings.groupby(['movie_id', 'title'])['rating'].agg(['mean', 'sum', 'count', 'std'])
 print('Films ratings info: \n%s' % dataRatings[:10])
@@ -55,36 +45,11 @@ print('My info ratings: \n%s' % myAvg[:10])
 print('\n==================================================================\n')
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-<<<<<<< HEAD
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count()))
-sortRatingsField = sortRatingsField.sort_values(by='COUNT', ascending=False)  
-print('My info sorted: \n%s' % sortRatingsField[:15])
-=======
-<<<<<<< HEAD
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values(by = 'COUNT', ascending=False)
-#print('My info sorted: \n%s' % sortRatingsField[:15])
-=======
-=======
->>>>>>> 92fc51fa78a6ef01da3917baf6e15edd65dd2884
 sortRatingsFieldEmmanuel = cloneDF(mergeRatings)
 sortRatingsFieldEmmanuel = sortRatingsFieldEmmanuel.groupby(['movie_id', 'title'])['rating'].agg(
     COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values(by='COUNT',  ascending=False) #test Emmanuel , sort('COUNT', ascending=False)
 print('My info sorted: \n%s' % sortRatingsFieldEmmanuel[:15])
-<<<<<<< HEAD
-print('test')
-=======
-
-# Essaie des modifications Deborah
-
-
 print('test')
 
-
->>>>>>> 01e98f7fcc97ae20c4e249c842ee0beb2b5ecdf4
->>>>>>> 22a35241b94ff71766ed79d0af0aa5c452267d18
->>>>>>> 92fc51fa78a6ef01da3917baf6e15edd65dd2884
+# Essai des modifications Deborah
+print('test')
